0c051a8117c14499bb62d00d6f8255db6ef71602
furage <71694701+furage@users.noreply.github.com>
2021-04-11 15:52:58 +0900

    Create adblock.js

---
+++ adblock.js
@@ -0,0 +1,14 @@
+(function(){
+    var e = document.createElement("script");
+    e.setAttribute("src","https://code.jquery.com/jquery-1.9.1.js");
+    document.body.append(e);
+})();
+jQuery.noConflict();
+(function($) {
+    var id = setInterval(function(){
+        $("[class*=blog_ad],#category_ranking,#pbp_ranking,#blog_news,#blog_ranking,#ad,#ld-floating-menu,.blogroll_ad_news").remove();
+    }, 500);
+    setTimeout(function(){
+        clearInterval(id);
+    },10*1000);
+})(jQuery);
