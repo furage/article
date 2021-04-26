a592826bdc6953759d9ce41ba309323cfb6a2618
furage <71694701+furage@users.noreply.github.com>
2021-04-27 08:12:29 +0900

    Update adblock.js

--- adblock.js
+++ adblock.js
@@ -8,6 +8,7 @@ jQuery.noConflict();
     var id = setInterval(function(){
         $("[class*=blog_ad],#category_ranking,#pbp_ranking,#blog_news,#blog_ranking,#ad,#ld-floating-menu,.blogroll_ad_news").remove();
         $(".blogroll-ad-text,.blogroll-ad-default").remove();
+        $(".article_mid_v2,#geniee_overlay").remove(); // ユーザー広告？
     }, 500);
     setTimeout(function(){
         clearInterval(id);
