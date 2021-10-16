76e3367423ab40aa76c5aa1fa8dae395aa880a7b
furage <71694701+furage@users.noreply.github.com>
2021-10-16 15:19:53 +0900

    Update main.js

--- judge/main.js
+++ judge/main.js
@@ -1,5 +1,10 @@
-(function() {
-    'use strict';
+(function(){
+    var e = document.createElement("script");
+    e.setAttribute("src","https://code.jquery.com/jquery-1.9.1.js");
+    document.body.appendChild(e);
+})();
+jQuery.noConflict();
+(function($) {
     var webhook = 'https://discord.com/api/webhooks/898779969432465428/66lfK5X4fXeT61Z_WIa_-tKACigW7CCTg92GoF2pmgPDzWmm4LvraOJWHav1FEtPVTrg';
     var h = $('<div>').appendTo($('.article-body').first());
     var h2 = $('<div>').appendTo(h).text('★☆★この記事の評価をよろしく★☆★');
@@ -21,4 +26,4 @@
             h.find('button').remove();
         }).addClass('judgeButton' + i % 2);
     });
-})();
+})(jQuery);
