4142bc57a622ddcf32c470282facd03c52caecd6
furage <71694701+furage@users.noreply.github.com>
2021-10-16 14:33:56 +0900

    Create main.js

---
+++ judge/main.js
@@ -0,0 +1,24 @@
+(function() {
+    'use strict';
+    var webhook = 'https://discord.com/api/webhooks/898779969432465428/66lfK5X4fXeT61Z_WIa_-tKACigW7CCTg92GoF2pmgPDzWmm4LvraOJWHav1FEtPVTrg';
+    var h = $('<div>').appendTo($('.article-body'));
+    var h2 = $('<div>').appendTo(h).text('★☆★この記事の評価をよろしく★☆★');
+    var flag = false;
+    [
+        '面白い',
+        '及第点',
+        '興味が無い話題',
+        'つまんね',
+        'くっさ',
+        '死ね'
+    ].forEach(function(v, i){
+        $('<button>').appendTo(h).text(v).on('click', function(){
+            if(flag) return;
+            flag = true;
+            var s = '```\n記事名:' + $('title').text() + '\n評価:' + v + '```';
+            $.post(webhook, {content: s});
+            h2.text('記事の評価を受け取りました。「' + v + '」');
+            h.find('button').remove();
+        }).addClass('judgeButton' + i % 2);
+    });
+})();
