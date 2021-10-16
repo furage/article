cb1d70e83c6752564f71a4bf990919f91e5515eb
furage <71694701+furage@users.noreply.github.com>
2021-10-16 14:59:20 +0900

    Update main.js

--- judge/main.js
+++ judge/main.js
@@ -1,9 +1,6 @@
 (function() {
     'use strict';
     var webhook = 'https://discord.com/api/webhooks/898779969432465428/66lfK5X4fXeT61Z_WIa_-tKACigW7CCTg92GoF2pmgPDzWmm4LvraOJWHav1FEtPVTrg';
-    var unique = 'judge1234567890';
-    if(window[unique]) return;
-    window[unique] = true;
     var h = $('<div>').appendTo($('.article-body').first());
     var h2 = $('<div>').appendTo(h).text('★☆★この記事の評価をよろしく★☆★');
     var flag = false;
